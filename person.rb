579ef7deb0c713c367df196340cbb88dabee9912
banlonjones <banlonjones@go-groups.net>
2022-04-04 16:52:35 +0100

    person, studen, teacher class implemented

---
+++ person.rb
@@ -0,0 +1,23 @@
+class Person
+  attr_reader :id
+  attr_accessor :name, :age
+
+  def initialize(age, name = 'Unknown', parent_permission = true)
+    @name = name
+    @age = age
+    @parent_permission = parent_permission
+    @id = Random.rand(1..1000)
+  end
+
+  def of_age?
+    return true if @age >= 18
+
+    false
+  end
+
+  def can_use_services?
+    return true if of_age? || @parent_permission
+
+    false
+  end
+end
